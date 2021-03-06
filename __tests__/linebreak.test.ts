4f869cbc1142fd873951591f28a49ce05d0706d1
Niek Palm <npalm@users.noreply.github.com>
2021-03-06 16:27:07 +0100

    chore: add sonar

--- __tests__/linebreak.test.ts
+++ __tests__/linebreak.test.ts
@@ -2,9 +2,9 @@ import * as lb from "../src/linebreak";
 
 describe("Verify line break utils.", () => {
   test("Test line break string to type conversion.", () => {
-    expect(lb.getLineBreakType("CR")).toEqual("CR");
     expect(lb.getLineBreakType("CRLF")).toEqual("CRLF");
     expect(lb.getLineBreakType("LF")).toEqual("LF");
+    expect(lb.getLineBreakType("CR")).toEqual("CR");
     expect(lb.getLineBreakType("unknown")).toEqual("LF");
   });
 
