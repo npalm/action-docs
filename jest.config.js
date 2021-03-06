df395d9b421b9a8d24f1db611e18dc912c80244e
Niek Palm <npalm@users.noreply.github.com>
2021-03-06 16:27:07 +0100

    chore: add sonar

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
+  collectCoverage: true, 
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
