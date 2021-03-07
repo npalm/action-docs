634cca28ed6e5b6aaf2ef404ecd1ef8a351184bc
Niek Palm <npalm@users.noreply.github.com>
2021-03-07 17:39:13 +0100

    chore: code cleanup (#25)

--- src/action-docs.ts
+++ src/action-docs.ts
@@ -18,6 +18,19 @@ interface ActionMarkdown {
   runs: string;
 }
 
+interface ActionYml {
+  name: string;
+  description: string;
+  inputs: ActionInputsOutputs;
+  outputs: ActionInputsOutputs;
+  runs: RunType;
+}
+
+interface RunType {
+  using: string;
+  main: string;
+}
+
 interface DefaultOptions {
   tocLevel: number;
   actionFile: string;
@@ -33,16 +46,30 @@ export const defaultOptions: DefaultOptions = {
   readmeFile: "README.md",
   lineBreaks: "LF",
 };
+
+type ActionInputsOutputs = Record<string, ActionInput | ActionOutput>;
+
 interface ActionInput {
   required?: boolean;
   description: string;
   default?: string;
 }
 
-function createMdTable(options: DefaultOptions, data: string[][]): string {
+interface ActionOutput {
+  description: string;
+}
+
+function createMdTable(
+  data: ActionInputsOutputs,
+  options: DefaultOptions,
+  type: "input" | "output"
+): string {
+  const tableData = getInputOutput(data, type);
+  const tableArray = tableData.headers.concat(tableData.rows);
+
   let result = "";
 
-  for (const line of data) {
+  for (const line of tableArray) {
     result = `${result}|`;
     for (const c of line) {
       result = `${result} ${c} |`;
@@ -80,6 +107,26 @@ export async function generateActionMarkdownDocs(
   return `${docs.description + docs.inputs + docs.outputs + docs.runs}`;
 }
 
+function generateActionDocs(options: DefaultOptions): ActionMarkdown {
+  const yml = load(readFileSync(options.actionFile, "utf-8"), {
+    json: true,
+  }) as ActionYml;
+
+  const inputMdTable = createMdTable(yml.inputs, options, "input");
+  const outputMdTable = createMdTable(yml.outputs, options, "output");
+
+  return {
+    description: createMarkdownSection(options, yml.description, "Description"),
+    inputs: createMarkdownSection(options, inputMdTable, "Inputs"),
+    outputs: createMarkdownSection(options, outputMdTable, "Outputs"),
+    runs: createMarkdownSection(
+      options,
+      `This action is an \`${yml.runs.using}\` action.`,
+      "Runs"
+    ),
+  };
+}
+
 async function updateReadme(
   options: DefaultOptions,
   text: string,
@@ -114,64 +161,34 @@ function createMarkdownSection(
     : "";
 }
 
-function generateActionDocs(options: DefaultOptions): ActionMarkdown {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const yml: any = load(readFileSync(options.actionFile, "utf-8"), {
-    json: true,
-  });
-
-  const inputData = getInputOutput(yml.inputs, "input");
-  const inputMdTable = createMdTable(
-    options,
-    inputData.headers.concat(inputData.rows)
-  );
-
-  const outputData = getInputOutput(yml.outputs, "output");
-  const outputMdTable = createMdTable(
-    options,
-    outputData.headers.concat(outputData.rows)
-  );
-
-  return {
-    description: createMarkdownSection(options, yml.description, "Description"),
-    inputs: createMarkdownSection(options, inputMdTable, "Inputs"),
-    outputs: createMarkdownSection(options, outputMdTable, "Outputs"),
-    runs: createMarkdownSection(
-      options,
-      `This action is an \`${yml.runs.using}\` action.`,
-      "Runs"
-    ),
-  };
-}
-
 function getInputOutput(
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  inputs: any,
+  data: ActionInputsOutputs,
   type: "input" | "output"
 ): { headers: string[][]; rows: string[][] } {
   const headers: string[][] = [];
   const rows: string[][] = [];
-  if (inputs !== undefined) {
-    headers[0] =
-      type === "input"
-        ? ["parameter", "description", "required", "default"]
-        : ["parameter", "description"];
-    headers[1] = Array(headers[0].length).fill("-");
+  if (data === undefined) {
+    return { headers, rows };
+  }
 
-    //let i = 0;
-    for (let i = 0; i < Object.keys(inputs).length; i++) {
-      const key = Object.keys(inputs)[i];
-      const input = inputs[key] as ActionInput;
-      rows[i] = [];
-      rows[i].push(key);
-      rows[i].push(input.description);
+  headers[0] =
+    type === "input"
+      ? ["parameter", "description", "required", "default"]
+      : ["parameter", "description"];
+  headers[1] = Array(headers[0].length).fill("-");
 
-      if (type === "input") {
-        rows[i].push(
-          input.required ? `\`${String(input.required)}\`` : "`false`"
-        );
-        rows[i].push(input.default ? input.default : "");
-      }
+  for (let i = 0; i < Object.keys(data).length; i++) {
+    const key = Object.keys(data)[i];
+    const value = data[key] as ActionInput;
+    rows[i] = [];
+    rows[i].push(key);
+    rows[i].push(value.description);
+
+    if (type === "input") {
+      rows[i].push(
+        value.required ? `\`${String(value.required)}\`` : "`false`"
+      );
+      rows[i].push(value.default ? value.default : "");
     }
   }
   return { headers, rows };
