fd431d7aedaac2380a3a91abbe908c6f28f80c35
Niek Palm <npalm@users.noreply.github.com>
2024-02-08 10:56:39 +0100

    chore: cleanup and ci improvements (#519)
    * chore: cleanup and ci improvements
    
    * chore: cleanup and ci improvements
    
    * chore: cleanup and ci improvements

--- jest.config.js
+++ jest.config.js
@@ -15,10 +15,10 @@ const config = {
   verbose: true,
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10,
+      statements: 100,
+      branches: 97.14,
+      functions: 95.23,
+      lines: 100,
     },
   },
 };
